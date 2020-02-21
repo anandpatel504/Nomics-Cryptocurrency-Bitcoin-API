d318b0bb2d7f11896d6614f085ee67d170047ea8
Anand <44016225+anandpatel504@users.noreply.github.com>
2020-02-21 16:27:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const CircularJSON = require('circular-json');
 app.use(express.json())
 
 var knex = require('./models/database');
-// const api_key = "f18f5cd0afd42cbfd6f6359510d62ebc";
 const api_key = process.env.api_key;
 
 // Task-1
